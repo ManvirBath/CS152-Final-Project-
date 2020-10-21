8c567f53c4ccd6b1e913cb36bcac224abd106619
sunnyxu2 <sunny.xu@sjsu.edu>
2020-10-20 22:15:40 -0700

    Added test environment to test character movement
    * Added new test sprites
    * Added basic character and player classes

---
+++ Treasure Hunt/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: ADD BETTER COMMENTS
+
+public class CharacterController : MonoBehaviour
+{
+    float speed = 6;
+    float walkAcceleration = 75;
+    float airAcceleration = 55;
+    float groundDeceleration = 70;
+    float jumpHeight = 3;
+
+    private CapsuleCollider2D boxCollider;
+    private Vector2 vel;
+    private bool grounded;
+
+    bool jumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = gameObject.GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyMovement();
+        TestCollisions();
+    }
+
+    
+    public void Jump()
+    {
+        if (grounded)
+        {
+            grounded = false;
+            vel.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+        }
+        //transform.Translate(vel * Time.deltaTime);
+    }
+
+    public void Move(float left_or_right)
+    {
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : 0;
+
+        if (left_or_right != 0)
+        {
+            vel.x = Mathf.MoveTowards(vel.x, speed * left_or_right, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            vel.x = Mathf.MoveTowards(vel.x, 0, deceleration * Time.deltaTime);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        if (grounded)
+        {
+            vel.y = 0;
+        }
+        vel.y += Physics2D.gravity.y * Time.deltaTime;
+
+        transform.Translate(vel * Time.deltaTime);
+    }
+
+
+    private void TestCollisions()
+    {
+        boxCollider.enabled = false;
+        Vector2 corner_left = new Vector2(transform.position.x - (boxCollider.size.y / 2), transform.position.y);
+        Vector2 corner_right = new Vector2(transform.position.x + (boxCollider.size.y / 2), transform.position.y);
+        RaycastHit2D targL = Physics2D.Raycast(corner_left, Vector2.down);
+        RaycastHit2D targR = Physics2D.Raycast(corner_right, Vector2.down);
+        boxCollider.enabled = true;
+        grounded = false;
+
+        if ((targL.collider != null && targL.distance < (boxCollider.size.y / 2) + 0.001) || (targR.collider != null && targR.distance < (boxCollider.size.y / 2) + 0.001))
+        {
+            float scalar = Math.Min(targL.distance, targR.distance);
+            if (targL.collider != null && targR.collider == null)
+            {
+                scalar = targL.distance;
+            }
+            else if (targL.collider == null && targR.collider != null)
+            {
+                scalar = targR.distance;
+            }
+            Vector2 expected = new Vector2(0, (boxCollider.size.y / 2) - scalar);
+            transform.Translate(expected);
+            grounded = true;
+        }
+
+        // Do overlap testing to see if the player has collided with something
+        // derived from: https://roystan.net/articles/character-controller-2d.html
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == boxCollider) continue;
+
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) != 0 && vel.y < 0)
+                {
+                    transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+                }
+            }
+        }
+    }
+}
---
+++ Treasure Hunt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// derived from: https://roystan.net/articles/character-controller-2d.html
+
+public class PlayerController : MonoBehaviour
+{
+    float moveInput;
+    CharacterController character;
+
+    private void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // control
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButton("Jump"))
+        {
+            // Calculate the velocity required to achieve the target jump height.
+            character.Jump();
+        }
+
+        character.Move(moveInput);
+    }
+}
